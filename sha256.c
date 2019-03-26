93b9cf8b6778d149a0bbfd5b8529b9f4bfe97ebb
Jomzi <james_ohanlon1@theory-of-algorithms.europe-west2-c.c.infra-rhino-229411.internal>
2019-03-26 13:43:59 +0000

    Added hash values, Sigma function, Initiliazed array and added new for loop

--- sha256.c
+++ sha256.c
@@ -2,12 +2,20 @@
 // The Secure Hash Algorithm, 256 bit version 
 // https://ws680.nist.gov/publication/get_pdf.cfm?pub_id=919060
 
-//The usual input/output header file.
+// The usual input/output header file.
 #include <stdio.h> 
 // For using fixed bit lenght integer.
 #include <stdint.h>
 
 void sha256();
+
+// See Section 4.1.2 for definitions.
+uint32_t sig0(uint32_t x);
+uint32_t sig1(uint32_t x);
+
+// See Section 3.2 for defintions.
+uint32_t rotr(uint32_t n, uint32_t x);
+uint32_t shr(uint32_t n, uint32_tx);
 
 int main(int argc, char *argv[]){
   
@@ -39,5 +47,64 @@ void sha256(){
 
   // The current message block.
   uint32_t M[16];
+  
+  // For looping.
+  int t;
+  
+  // From page 22, W[t] = M[t] for 0 <= t <= 15.
+  for(t = o; t < 16; t++)
+    W[t] = M[t];
+
+  // From page 22, W[t] = ...
+  for (t = 16; t < 64; t++)
+    sig1(W[t-2]) + W[t-7] + sig0(W[t-15]) + W[t-16];
+  
+  // Initialis a, b, c, .., h as per Step 2, Page 22.
+  a = H[0]; b = H[1]; c = H[2]; d = H[3];
+  e = H[4]; f = H[5]; g = H[6]; h = H[7];
+  
+  // Step 3.
+  for (t = 0; t < 64; t++){
+    T1 = h + SIG_1(e) +Ch(e, f, g) + W[t];
+    T2 = SIG_0(a) + Maj(a, b, c);
+    h = g;
+    g = f;
+    f = e;
+    e = d + T1;
+    d = c;
+    c = b; 
+    b = a;  
+    a = T1 + T2;  
+
+}
+
+ // Step 4.
+ H[0] = a + H[0];
+ H[1] = b + H[1];
+ H[2] = c + H[2];
+ H[3] = d + H[3];
+ H[4] = e + H[4];
+ H[5] = f + H[5];
+ H[6] = g + H[6];
+ H[7] = h + H[7];
 
 } 
+
+// See Section 3.2 for defintions.
+uint32_t rotr(uint32_t n, uint32_t x){
+  return(x >> n) | (x << (32 - n));
+}
+
+uint32_t shr(uint32_t n, uint32_t x){
+  return(x >> n);
+}
+
+uint32_t sig0(uint32_t x){
+  // See Sections 3.2 and 4.1.2 for definitions.	
+  return(rotr(7, x) ^ rotr(18, x) ^ shr(3, x));
+}
+
+uint32_t sig1(uint32_t x){
+  // See Sections 3.2 and 4.1.2 for definitions.
+  return (rotr(17, x) ^ rotr(19, x) ^ shr(10, x));
+}
