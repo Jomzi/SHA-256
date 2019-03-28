9cde92aca13e58aede7bb8b3e17d355f961f0e84
Jomzi <james_ohanlon1@theory-of-algorithms.europe-west2-c.c.infra-rhino-229411.internal>
2019-03-28 19:02:43 +0000

    Ran padfile and getting the correct output.

--- padfile.c
+++ padfile.c
@@ -1,16 +1,28 @@
 #include <stdio.h>
 #include <stdint.h>
 
+union  msgblock {
+  uint8_t   e[64];
+  uint32_t  t[16];
+  uint64_t  s[8];
+};
+
+
 int main(int argc, char *argv[]){
+
+  union msgblock M;
+
+  uint64_t nobytes;  
 
   FILE* f;
 
-  char c;
-
   f = fopen(argv[1], "r");
-
-  printf("%c\n", fread(&c, 1, 1, f));
-
+  
+  while (!feof(f)){
+    nobytes = fread(M.e, 1, 64, f);
+    printf("%llu\n", nobytes);
+  }
+ 
   fclose(f);
 
   return 0;  
