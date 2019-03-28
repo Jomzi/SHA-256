9ba823d3f7141ce746a010eea7d07738535711fd
Jomzi <james_ohanlon1@theory-of-algorithms.europe-west2-c.c.infra-rhino-229411.internal>
2019-03-28 22:30:08 +0000

    Finishing out the padding.

--- padfile.c
+++ padfile.c
@@ -7,6 +7,8 @@ union  msgblock {
   uint64_t  s[8];
 };
 
+enum status {READ, PAD0, PAD1, FINISH};
+
 
 int main(int argc, char *argv[]){
 
@@ -14,13 +16,17 @@ int main(int argc, char *argv[]){
 
   uint64_t nobits = 0;
 
-  uint64_t nobytes;  
+  uint64_t nobytes;
+
+  enum status S = READ;  
 
   FILE* f;
 
   f = fopen(argv[1], "r");
+
+  int i;
   
-  while (!feof(f)){
+  while (S == READ) {
     nobytes = fread(M.e, 1, 64, f);
     printf("Read %2llu bytes\n", nobytes); 
     nobits = nobits + (nobytes * 8);
@@ -32,9 +38,27 @@ int main(int argc, char *argv[]){
         M.e[nobytes] = 0x00;	
       }
       M.s[7] = nobits;
-    }
+      S = FINISH;
+    } else if (nobytes < 64){
+      S = PAD0;
+      M.e[nobytes] = 0x80;
+      while (nobytes < 64){
+	nobytes = nobytes + 1;
+        M.e[nobytes] = 0x00;
+      }	
+    } else if (feof(f)) {
+      S = PAD1;
+    }      
   }
- 
+
+  if (S == PAD0 || S == PAD1) {
+    for (i = 0; i < 56; i++)
+      M.e[i] = 0x00;
+    M.s[7] = nobits;
+  }
+  if (S == PAD1)
+    M.e[0] = 0x80;	
+
   fclose(f);
 
   for (int i = 0; i < 64; i++)
