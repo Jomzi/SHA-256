bdc744010a6257cfe51f36bfccd89080301254bd
Jomzi <james_ohanlon1@theory-of-algorithms.europe-west2-c.c.infra-rhino-229411.internal>
2019-03-28 21:05:51 +0000

    When I run the padfile it checks the bits to see if they are over or under 55 and prints out message

--- padfile.c
+++ padfile.c
@@ -12,6 +12,8 @@ int main(int argc, char *argv[]){
 
   union msgblock M;
 
+  uint64_t nobits = 0;
+
   uint64_t nobytes;  
 
   FILE* f;
@@ -20,10 +22,24 @@ int main(int argc, char *argv[]){
   
   while (!feof(f)){
     nobytes = fread(M.e, 1, 64, f);
-    printf("%llu\n", nobytes);
+    printf("Read %2llu bytes\n", nobytes); 
+    nobits = nobits + (nobytes * 8);
+    if (nobytes < 56) { 
+      printf("I've found a block with less than 55 bytes!\n");  
+      M.e[nobytes] = 0x80;
+      while (nobytes < 56){
+	nobytes = nobytes + 1;
+        M.e[nobytes] = 0x00;	
+      }
+      M.s[7] = nobits;
+    }
   }
  
   fclose(f);
+
+  for (int i = 0; i < 64; i++)
+    printf("%x ", M.e[i]);
+  printf("\n");  
 
   return 0;  
 }	
