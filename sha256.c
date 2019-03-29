e485ba1e65d6232c65f982b9b8a290794e096afa
Jomzi <james_ohanlon1@theory-of-algorithms.europe-west2-c.c.infra-rhino-229411.internal>
2019-03-29 20:00:48 +0000

    Tidied upcode and shortened the lines.

--- sha256.c
+++ sha256.c
@@ -97,14 +97,10 @@ void sha256(FILE *f){
   // The Hash value (Section 6.2).
   // The value come from Section 5.3.3.
   uint32_t H[8] = {
-      0x6a09e667
-    , 0xbb67ae85
-    , 0x3c6ef372
-    , 0xa54ff53a
-    , 0x510e527f	       
-    , 0x9b05688c
-    , 0x1f83d9ab
-    , 0x5be0cd19
+      0x6a09e667, 0xbb67ae85
+    , 0x3c6ef372, 0xa54ff53a
+    , 0x510e527f, 0x9b05688c
+    , 0x1f83d9ab, 0x5be0cd19
   };
   
 
@@ -113,48 +109,42 @@ void sha256(FILE *f){
 
   while (nextmsgblock(f, M, S, nobits)){
   
-  // Loop through message blocks as per page 22.
-  for(i = 0; i < 1; i++){
   
-  
-  // From page 22, W[t] = M[t] for 0 <= t <= 15.
-  for(t = 0; t < 16; t++)
-    W[t] = M.t[t];
+    // From page 22, W[t] = M[t] for 0 <= t <= 15.
+    for(t = 0; t < 16; t++)
+      W[t] = M.t[t];
 
-  // From page 22, W[t] = ...
-  for (t = 16; t < 64; t++)
-    W[t] =  sig1(W[t-2]) + W[t-7] + sig0(W[t-15]) + W[t-16];
+    // From page 22, W[t] = ...
+    for (t = 16; t < 64; t++)
+      W[t] =  sig1(W[t-2]) + W[t-7] + sig0(W[t-15]) + W[t-16];
   
-  // Initialis a, b, c, .., h as per Step 2, Page 22.
-  a = H[0]; b = H[1]; c = H[2]; d = H[3];
-  e = H[4]; f = H[5]; g = H[6]; h = H[7];
+    // Initialis a, b, c, .., h as per Step 2, Page 22.
+    a = H[0]; b = H[1]; c = H[2]; d = H[3];
+    e = H[4]; f = H[5]; g = H[6]; h = H[7];
   
-  // Step 3.
-  for (t = 0; t < 64; t++){
-    T1 = h + SIG1(e) + Ch(e, f, g) + K[t] +  W[t];
-    T2 = SIG0(a) + Maj(a, b, c);
-    h = g;
-    g = f;
-    f = e;
-    e = d + T1;
-    d = c;
-    c = b; 
-    b = a;  
-    a = T1 + T2;  
-  }
+    // Step 3.
+    for (t = 0; t < 64; t++){
+      T1 = h + SIG1(e) + Ch(e, f, g) + K[t] +  W[t];
+      T2 = SIG0(a) + Maj(a, b, c);
+      h = g;
+      g = f;
+      f = e;
+      e = d + T1;
+      d = c;
+      c = b; 
+      b = a;  
+      a = T1 + T2;  
+    } 
 
-  // Step 4.
-  H[0] = a + H[0];
-  H[1] = b + H[1];
-  H[2] = c + H[2];
-  H[3] = d + H[3];
-  H[4] = e + H[4];
-  H[5] = f + H[5];
-  H[6] = g + H[6];
-  H[7] = h + H[7];
+    // Step 4.
+    H[0] = a + H[0]; H[1] = b + H[1];
+    H[2] = c + H[2]; H[3] = d + H[3];
+    H[4] = e + H[4]; H[5] = f + H[5];
+    H[6] = g + H[6]; H[7] = h + H[7];
 
-  }
-  printf("%x %x %x %x %x %x %x %x\n", H[0], H[1], H[2], H[3], H[4], H[5], H[6], H[7]);
+   }
+
+   printf("%x %x %x %x %x %x %x %x\n", H[0], H[1], H[2], H[3], H[4], H[5], H[6], H[7]);
 } 
 
 
