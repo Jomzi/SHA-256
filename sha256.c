a33429374241a2e5f41d2c60246a5112da7a24d0
Jomzi <james_ohanlon1@theory-of-algorithms.europe-west2-c.c.infra-rhino-229411.internal>
2019-03-29 14:17:31 +0000

    Started the padding for the sha256 file.

--- sha256.c
+++ sha256.c
@@ -7,8 +7,17 @@
 // For using fixed bit lenght integer.
 #include <stdint.h>
 
+// Represents a message block.
+union msgblock {
+  uint8_t  e[64];
+  uint32_t t[16];
+  uint64_t s[8];
+};
 
-void sha256();
+// A flag for where we are in reading the file.
+enum status {READ, PAD0, PAD1, FINISH};
+
+
 
 // See Section 4.1.2 for definitions.
 uint32_t sig0(uint32_t x);
@@ -26,17 +35,34 @@ uint32_t SIG1(uint32_t x);
 uint32_t Ch(uint32_t x, uint32_t y, uint32_t z);
 uint32_t Maj(uint32_t x, uint32_t y, uint32_t z);
 
+void sha256(FILE *f);
 
+// Retrieves the next message block.
+int nextmsgblock(FILE *f, union msgblock *M, enum status *S, int *nobits);
+
+// Start of the show.
 int main(int argc, char *argv[]){
+
+  FILE* f;
+  f = fopen(argv[1], "r");  
   
-  sha256();
+  sha256(f);
 
   return 0;
 }  
 
 
 void sha256(){
-  
+   
+  // The current message block
+  union msgblock M;
+
+  // The number of bits read from  the file.
+  uint64_t nobits = 0;
+
+  // The status of the message blocks, in terms of padding.
+  enum status S = READ;
+
   // The K constants, defined in Section 4.2.2.
   uint32_t K[] = {
     0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5,
@@ -77,13 +103,12 @@ void sha256(){
     , 0x1f83d9ab
     , 0x5be0cd19
   };
-
-  // The current message block.
-  uint32_t M[16] =  {0, 0, 0, 0, 0, 0, 0, 0};
   
 
   // For looping.
   int i,t;
+
+  while (nextmsgblock(f, M, S, nobits)){
   
   // Loop through message blocks as per page 22.
   for(i = 0; i < 1; i++){
@@ -167,3 +192,24 @@ uint32_t Ch(uint32_t x, uint32_t y, uint32_t z){
 uint32_t Maj(uint32_t x, uint32_t y, uint32_t z){
   return ((x & y) ^ (x & z));	
 }
+
+int nextmsgblock(FILE *f, union msgblock *M, enum status *S, int *nobits);
+  union msgblock M;
+
+  uint64_t nobits = 0;
+
+  uint64_t nobytes;
+
+  enum status S = READ;
+
+  int i;
+
+  while (S == READ) {
+    nobytes = fread(M.e, 1, 64, f);
+    printf("Read %2llu bytes\n", nobytes);
+    nobits = nobits + (nobytes * 8);
+    if (nobytes < 56){
+      printf("I've found a block with less than 55 bytes!\n");
+      M.e[nobytes] = 0x80;
+      while (nobytes < 56){
+        nobytes = nobytes + 1;
